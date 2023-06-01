4bb6732fb0cffebbb4b7c33f3f1ccfd803af6990
edgeless <lexicalsphere@gmail.com>
2023-06-01 04:17:26 +0000

    wip

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,31 @@
 <script>
 
-	let histories = ["test", "hoge"]
+	let gameSetting = "\
+	aaa\
+	";
+	let histories = [
+		{id: -1, content:"GM発言"},
+		{id: 0, content:"PL0発言"},
+		{id: 1, content:"PL1発言"}
+	]
 	let playerNum = 3;
 	let loading = false;
+	let stat = ""
+	let instruction = "";
+	let lastMessage = {};
+	/**
+	 * @type {never[]}
+	 */
+	let players = [];
 
 	/**
 	 * @param {string} message
+	 * @param {number} id
+	 * @param {any} from
 	 */
-	async function sendChatToPlayer(message){
+	async function proceedPlayer(from, id, message){
 		loading = true;
+		stat = `player ${id} is thinking...`;
 		fetch('https://trpgpt-player.vercel.app/api/chat', {
 			method: 'POST',
 			mode: 'cors', 
@@ -19,13 +36,19 @@
 			},
 			redirect: 'follow',
 			referrerPolicy: 'no-referrer', 
-			body: JSON.stringify(message) 
-		}).then((res) => res.text())// FIXME to json
+			body: JSON.stringify({"from": from, "id": id, "content": message}) 
+		}).then((res) => res.json())
 		.then((data) => {
 			loading = false;
-			histories.push(data);
-			histories = histories})
-		.catch((e) => {
+			histories.push(data.content);
+			histories = histories;
+			if(lastMessage.strength < data.strength) {
+				lastMessage = data
+			}
+			loading = false;
+			stat = "";
+		}).catch((e) => {
+			console.log("failed in proceedPlayer");
 			console.log(e);
 		})
 	}
@@ -33,9 +56,10 @@
 	/**
 	 * @param {string} message
 	 */
-	async function sendChatToGM(message){
+	async function proceedGM(message){
 		loading = true;
-		fetch('https://trpgpt-gm.vercel.app/api/chat', {
+		stat = `GM thinking...`;
+		fetch('https://trpgpt-gm.vercel.app/api/proceed', {
 			method: 'POST',
 			mode: 'cors', 
 			cache: 'no-cache', 
@@ -48,18 +72,108 @@
 			body: JSON.stringify(message) 
 		}).then((res) => res.json()) 
 		.then((data) => {
-
 			loading = false;
+			stat = ""
 			histories.push(data);
 			histories = histories})
 		.catch((e) => {
+			console.log("failed in proceedGM");
+			console.log(e);
+		})
+	}
+
+	async function initSession(){
+		loading = true;
+		stat = "initializing the session...";
+		console.log("call initSession");
+
+		fetch('https://trpgpt-gm.vercel.app/api/init', {
+			method: 'POST',
+			mode: 'cors', 
+			cache: 'no-cache', 
+			//credentials: 'same-origin', 
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			redirect: 'follow',
+			referrerPolicy: 'no-referrer', 
+			body: JSON.stringify({"content": gameSetting}) 
+		}).then((res) => res.json())
+		.then((data) => {
+			loading = false;
+			stat = "";
+			console.log(data);
+			instruction = data.content;
+		})
+		.catch((e) => {
+			console.log("failed in initSession");
+			console.log(e);
+		})
+	}
+
+	/**
+	 * @param {string | number | undefined} [id]
+	 */
+	async function createCharacter(id){
+		loading = true;
+		stat = `Creating a Character ${id}`
+		console.log("call createCharacter");
+
+		fetch('https://trpgpt-player.vercel.app/api/init', {
+			method: 'POST',
+			mode: 'cors', 
+			cache: 'no-cache', 
+			//credentials: 'same-origin', 
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			redirect: 'follow',
+			referrerPolicy: 'no-referrer', 
+			body: JSON.stringify({"text": instruction}) 
+		}).then((res) => res.json()) 
+		.then((data) => {
+			console.log(data);
+			loading = false;
+			stat = "";
+			players[id] = data.content
+		}).catch((e) => {
+			console.log(e);
+			console.log(e.message());
+		})
+	}
+
+	async function setUsers(){
+		loading = true;
+		stat = "GM thinking about players..."
+		console.log("call startSession");
+
+		fetch('https://trpgpt-gm.vercel.app/api/userset', {
+			method: 'POST',
+			mode: 'cors', 
+			cache: 'no-cache', 
+			//credentials: 'same-origin', 
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			redirect: 'follow',
+			referrerPolicy: 'no-referrer', 
+			body: JSON.stringify({"players": players}) 
+		}).then((res) => res.json())
+		.then((data) => {
+			loading = false;
+			stat = "";
+		})
+		.catch((e) => {
+			console.log("failed in startSession");
 			console.log(e);
 		})
 	}
 
 	async function startSession(){
 		loading = true;
-		fetch('https://trpgpt-gm.vercel.app/api/init', {
+		console.log("call startSession");
+
+		fetch('https://trpgpt-gm.vercel.app/api/start', {
 			method: 'POST',
 			mode: 'cors', 
 			cache: 'no-cache', 
@@ -72,43 +186,55 @@
 			body: JSON.stringify({}) 
 		}).then((res) => res.json()) // FIXME to json
 		.then((data) => {
-
-			loading = false;
-			histories.push(data);
-			histories = histories})
+			console.log(data);
+			histories.push(data.content);
+			lastMessage = data;
+			lastMessage.strength = 1;
+			lastMessage.id = -1;
+		})
 		.catch((e) => {
+			console.log("failed in startSession");
 			console.log(e);
 		})
 	}
 
-
 	async function start() {
-		histories.push("-----");
+		console.log(playerNum);
+		histories = histories;
+		await initSession();
+		for(let i=0;  i<playerNum; i++) {
+			await createCharacter();
+		}
+		await setUsers();
 		await startSession();
 	}
 
 	async function next() {
-		histories.push("-----");
-		await sendChatToGM("");
-		await sendChatToPlayer("");
+		await proceedGM("");
+		for(let i=0;  i<playerNum; i++) {
+			await proceedPlayer(lastMessage.id, i, lastMessage.content);
+		}
+		histories.push({lastMessage})
 	}
 
 </script>
 
+{#if loading}
+  <div>{stat}</div>
+{/if}
+
 <section>
 	<div>
 		{#each histories as utterance}
-			<p> {utterance} </p>
+			<pre> {utterance.id}: {utterance.content} </pre>
 		{/each}
 	</div>
 
+	<input type="text" placeholder="Player Num" bind:value={playerNum} 
+		class="input input-bordered w-full max-w-xs" />
 	<button on:click={start}> start </button>
 	<button on:click={next}> next </button>
 </section>
-
-{#if loading}
-  <div>Loading...</div>
-{/if}
 
 <style>
 	section {
