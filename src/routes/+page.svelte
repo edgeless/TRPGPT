2d164639b5f11a3eab722b42b6db4bffbb9b5b7d
edgeless <lexicalsphere@gmail.com>
2023-06-01 06:43:52 +0000

    WIP

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,15 @@
 <script>
-
-	let gameSetting = "\
-	ダンジョンもので、地下3階のボスを倒せば終了です。\
-	";
+	let gameSetting =
+		'ダンジョンもので、地下3階のボスを倒せば終了です。';
 	let histories = [
-		{id: -1, content:"GM発言"},
-		{id: 0, content:"PL0発言"},
-		{id: 1, content:"PL1発言"}
-	]
+		{ id: -1, content: 'GM発言' },
+		{ id: 0, content: 'PL0発言' },
+		{ id: 1, content: 'PL1発言' }
+	];
 	let playerNum = 3;
 	let loading = false;
-	let stat = ""
-	let instruction = "";
+	let stat = '';
+	let instruction = '';
 	let lastMessage = {};
 	let gameStarted = false;
 
@@ -25,199 +23,208 @@
 	 * @param {number} id
 	 * @param {any} from
 	 */
-	async function proceedPlayer(from, id, message){
+	async function proceedPlayer(from, id, message) {
 		loading = true;
 		stat = `player ${id} is thinking...`;
-		return fetch('https://trpgpt-player.vercel.app/api/chat', {
+		return fetch('https://trpgpt-player-gamma.vercel.app/api/chat', {
 			method: 'POST',
-			mode: 'cors', 
-			cache: 'no-cache', 
-			//credentials: 'same-origin', 
+			mode: 'cors',
+			cache: 'no-cache',
+			//credentials: 'same-origin',
 			headers: {
 				'Content-Type': 'application/json'
 			},
 			redirect: 'follow',
-			referrerPolicy: 'no-referrer', 
-			body: JSON.stringify({"from": from, "id": id, "content": message}) 
-		}).then((res) => res.json())
-		.then((data) => {
-			loading = false;
-			histories.push(data.content);
-			histories = histories;
-			if(lastMessage.strength < data.strength) {
-				lastMessage = data
-			}
-			loading = false;
-			stat = "";
-		}).catch((e) => {
-			console.log("failed in proceedPlayer");
-			console.log(e);
+			referrerPolicy: 'no-referrer',
+			body: JSON.stringify({ from: from, id: id, content: message })
 		})
+			.then((res) => res.json())
+			.then((data) => {
+				loading = false;
+				histories.push(data.content);
+				histories = histories;
+				if (lastMessage.strength < data.strength) {
+					lastMessage = data;
+				}
+				loading = false;
+				stat = '';
+			})
+			.catch((e) => {
+				console.log('failed in proceedPlayer');
+				console.log(e);
+			});
 	}
 
 	/**
 	 * @param {string} message
 	 */
-	async function proceedGM(){
+	async function proceedGM() {
 		loading = true;
 		stat = `GM thinking...`;
 		return fetch('https://trpgpt-gm.vercel.app/api/proceed', {
 			method: 'POST',
-			mode: 'cors', 
-			cache: 'no-cache', 
-			//credentials: 'same-origin', 
+			mode: 'cors',
+			cache: 'no-cache',
+			//credentials: 'same-origin',
 			headers: {
 				'Content-Type': 'application/json'
 			},
 			redirect: 'follow',
-			referrerPolicy: 'no-referrer', 
-			body: JSON.stringify({"from": lastMessage.id, "content": lastMessage.content}) 
-		}).then((res) => res.json()) 
-		.then((data) => {
-			loading = false;
-			stat = ""
-			histories.push(data);
-			histories = histories})
-		.catch((e) => {
-			console.log("failed in proceedGM");
-			console.log(e);
-			return proceedGM();
+			referrerPolicy: 'no-referrer',
+			body: JSON.stringify({ from: lastMessage.id, content: lastMessage.content })
 		})
+			.then((res) => res.json())
+			.then((data) => {
+				loading = false;
+				stat = '';
+				histories.push(data);
+				histories = histories;
+			})
+			.catch((e) => {
+				console.log('failed in proceedGM');
+				console.log(e);
+				return proceedGM();
+			});
 	}
 
 	// curl -X POST -H "Content-Type: application/json" --data '{"content":"ダンジョンものです"}'  https://trpgpt-gm.vercel.app/api/init
-	async function initSession(){
+	async function initSession() {
 		gameStarted = true;
 		loading = true;
-		stat = "initializing the session...";
-		console.log("call initSession");
-		
-		
+		stat = 'initializing the session...';
+		console.log('call initSession');
+
 		return fetch('https://trpgpt-gm.vercel.app/api/init', {
 			method: 'POST',
-			mode: 'cors', 
-			cache: 'no-cache', 
-			//credentials: 'same-origin', 
+			mode: 'cors',
+			cache: 'no-cache',
+			//credentials: 'same-origin',
 			headers: {
 				'Content-Type': 'application/json'
 			},
 			redirect: 'follow',
-			referrerPolicy: 'no-referrer', 
-			body: JSON.stringify({"content": gameSetting}) 
-		}).then((res) => res.json())
-		.then((data) => {
-			loading = false;
-			stat = "";
-			console.log(data);
-			instruction = data.content;
+			referrerPolicy: 'no-referrer',
+			body: JSON.stringify({ content: gameSetting })
 		})
-		.catch((e) => {
-			console.log("failed in initSession");
-			console.log(e);
-			return initSession();
-		})
+			.then((res) => res.json())
+			.then((data) => {
+				loading = false;
+				stat = '';
+				console.log(data);
+				instruction = data.content;
+			})
+			.catch((e) => {
+				console.log('failed in initSession');
+				console.log(e);
+				return initSession();
+			});
 	}
 
 	/**
 	 * @param {string | number | undefined} [id]
 	 */
-	async function createCharacter(id){
+	async function createCharacter(id) {
 		loading = true;
-		stat = `Creating a Character ${id}`
-		console.log("call createCharacter");
+		stat = `Creating a Character ${id}`;
+		console.log('call createCharacter');
 
-		return fetch('https://trpgpt-player.vercel.app/api/init', {
+		return fetch('https://trpgpt-player-gamma.vercel.app/api/init', {
 			method: 'POST',
-			mode: 'cors', 
-			cache: 'no-cache', 
-			//credentials: 'same-origin', 
+			mode: 'cors',
+			cache: 'no-cache',
+			//credentials: 'same-origin',
 			headers: {
 				'Content-Type': 'application/json'
 			},
 			redirect: 'follow',
-			referrerPolicy: 'no-referrer', 
-			body: JSON.stringify({"text": instruction}) 
-		}).then((res) => res.json()) 
-		.then((data) => {
-			console.log(data);
-			loading = false;
-			stat = "";
-			players.push(data.content);
-			players = players
-		}).catch((e) => {
-			console.log("failed in createCharacter");
-			console.log(e);
-			createCharacter(id);
+			referrerPolicy: 'no-referrer',
+			body: JSON.stringify({ text: instruction })
 		})
+			.then((res) => res.json())
+			.then((data) => {
+				console.log(data);
+				loading = false;
+				stat = '';
+				players.push(data.content);
+				players = players;
+			})
+			.catch((e) => {
+				console.log('failed in createCharacter');
+				console.log(e);
+				createCharacter(id);
+			});
 	}
 
-	async function setUsers(){
+	// $ curl -X POST -H "Content-Type: application/json" --data '{"content":"ダンジョンものです"}'  https://trpgpt-gm.vercel.app/api/init
+	async function setUsers() {
 		loading = true;
-		stat = "GM thinking about players..."
-		console.log("call startSession");
+		stat = 'GM thinking about players...';
+		console.log('call startSession');
 
 		return fetch('https://trpgpt-gm.vercel.app/api/userset', {
 			method: 'POST',
-			mode: 'cors', 
-			cache: 'no-cache', 
-			//credentials: 'same-origin', 
+			mode: 'cors',
+			cache: 'no-cache',
+			//credentials: 'same-origin',
 			headers: {
 				'Content-Type': 'application/json'
 			},
 			redirect: 'follow',
-			referrerPolicy: 'no-referrer', 
-			body: JSON.stringify({"players": players}) 
-		}).then((res) => res.json())
-		.then((data) => {
-			loading = false;
-			stat = "";
+			referrerPolicy: 'no-referrer',
+			body: JSON.stringify({ players: players })
 		})
-		.catch((e) => {
-			console.log("failed in setUsers");
-			console.log(e);
-			setUsers();
-		})
+			.then((res) => res.json())
+			.then((data) => {
+				loading = false;
+				stat = '';
+			})
+			.catch((e) => {
+				console.log('failed in setUsers');
+				console.log(e);
+				setUsers();
+			});
 	}
 
-	async function startSession(){
+	async function startSession() {
 		loading = true;
-		stat = "starting session..."
-		console.log("call startSession");
+		stat = 'starting session...';
+		console.log('call startSession');
 
 		return fetch('https://trpgpt-gm.vercel.app/api/start', {
 			method: 'POST',
-			mode: 'cors', 
-			cache: 'no-cache', 
-			//credentials: 'same-origin', 
+			mode: 'cors',
+			cache: 'no-cache',
+			//credentials: 'same-origin',
 			headers: {
 				'Content-Type': 'application/json'
 			},
 			redirect: 'follow',
-			referrerPolicy: 'no-referrer', 
-			body: JSON.stringify({}) 
-		}).then((res) => res.json())
-		.then((data) => {
-			console.log(data);
-			loading = false;
-			stat = "";
-			lastMessage = data;
-			lastMessage.strength = 1;
-			lastMessage.id = -1;
-			histories.push(lastMessage);
-			histories = histories
+			referrerPolicy: 'no-referrer',
+			body: JSON.stringify({})
 		})
-		.catch((e) => {
-			console.log("failed in startSession");
-			console.log(e);
-			startSession();
-		})
+			.then((res) => res.json())
+			.then((data) => {
+				console.log(data);
+				loading = false;
+				stat = '';
+				lastMessage = data;
+				lastMessage.strength = 1;
+				lastMessage.id = -1;
+				histories.push(lastMessage);
+				histories = histories;
+			})
+			.catch((e) => {
+				console.log('failed in startSession');
+				console.log(e);
+				startSession();
+			});
 	}
 
 	async function start() {
 		console.log(playerNum);
 		histories = histories;
 		await initSession();
-		for(let i=0;  i<playerNum; i++) {
+		for (let i = 0; i < playerNum; i++) {
 			await createCharacter(i);
 		}
 		await setUsers();
@@ -226,76 +233,101 @@
 
 	async function next() {
 		await proceedGM();
-		if(lastMessage.content != "") {
-
-			for(let i=0;  i<playerNum; i++) {
+		if (lastMessage.content != '') {
+			for (let i = 0; i < playerNum; i++) {
 				await proceedPlayer(lastMessage.id, i, lastMessage.content);
 			}
-			histories.push({lastMessage})
+			histories.push({ lastMessage });
 			histories = histories;
 		}
 	}
-
 </script>
 
 {#if loading}
-<div class="toast">
-	<div class="alert alert-info">
-	  <span>{stat}</span>
+	<div class="toast">
+		<div class="alert alert-info">
+			<span>{stat}</span>
+		</div>
 	</div>
-  </div>
 {/if}
 
-Game setting:
-{instruction}
+<div class="hero  bg-base-200">
+	<div class="hero-content text-center">
+	  <div class="max-w-md">
+		<h1 class="text-5xl font-bold">Game Settings</h1>
+		<p class="py-6">{instruction}</p>
+	  </div>
+	</div>
+  </div>
 
 Players:
 <section>
 	<div class="overflow-x-auto">
 		<table class="table">
-		  <!-- head -->
-		  <thead>
-			<tr>
-			  <th></th>
-			  <th>id</th>
-			  <th>description</th>
-			</tr>
-		  </thead>
-		  <tbody>
-			{#each players as player,i}
-			<tr>
-			  <th>1</th>
-			  <td>{i}</td>
-			  <td>{player}</td>
-			</tr>
-			{/each}
-		  </tbody>
+			<!-- head -->
+			<thead>
+				<tr>
+					<th />
+					<th>id</th>
+					<th>description</th>
+				</tr>
+			</thead>
+			<tbody>
+				{#each players as player, i}
+					<tr>
+						<th>1</th>
+						<td>{i}</td>
+						<td>{player}</td>
+					</tr>
+				{/each}
+			</tbody>
 		</table>
-	  </div>
+	</div>
 </section>
 
 <section>
 	{#each histories as utterance}
-	<div class="chat chat-start w-full">
-		<div class="chat-image avatar">
-			<div class="w-10 rounded-full">
-				<img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
-		  	</div>
+		<div class="chat chat-start w-full">
+			<div class="chat-image avatar">
+				<div class="w-10 rounded-full">
+					<img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+				</div>
+			</div>
+			<div class="chat-bubble w-30">{utterance.content}</div>
 		</div>
-		<div class="chat-bubble w-30">{utterance.content}</div>
-	</div>
 	{/each}
 
-	{#if !gameStarted}
-	<input type="text" placeholder="Game Config" bind:value={gameSetting} 
-		class="input input-bordered w-full max-w-xs" />
-	<input type="text" placeholder="Player Num" bind:value={playerNum} 
-		class="input input-bordered w-full max-w-xs" />
-	<button on:click={start}> start </button>
-	{/if}
-	{#if !loading}
-	<button on:click={next}> next </button>
-	{/if}
+	<div class="">
+		{#if !gameStarted}
+			<div class="form-control w-full">
+				<label class="label">
+					<span class="label-text">Game Config</span>
+					<input
+						type="text"
+						placeholder="Game Config"
+						bind:value={gameSetting}
+						class="input input-bordered w-full max-w-xs"
+					/>
+				</label>
+			</div>
+			<div class="form-control w-full max-w-xs">
+				<label class="label">
+					<span class="label-text">Player Num</span>
+					<input
+						type="number"
+						placeholder="Player Num"
+						bind:value={playerNum}
+						class="input input-bordered w-full max-w-xs"
+					/>
+				</label>
+			</div>
+
+			<button class="btn" on:click={start}> start </button>
+		{/if}
+		{#if !loading}
+			<button class="btn" on:click={next}> next </button>
+		{/if}
+	</div>
 </section>
 
 <style>
@@ -306,5 +338,4 @@ Players:
 		align-items: center;
 		flex: 0.6;
 	}
-
 </style>
